515dad0a17d510c4ed12f96f539badb2f288a576
Samuel <samuelumoren365@gmail.com>
2022-12-12 22:49:28 +0100

    made some changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "testamplifygatsby": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
